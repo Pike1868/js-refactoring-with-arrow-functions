712d945ffa1cd67477ca304c874df4c56efdddb5
Pike1868 <luis_14mendoza@protonmail.com>
2023-04-17 10:17:27 -0400

    adding in my solutions to a new branch

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ function double(arr){
 */
 
 //ES2015 Arrow Function Shorthand
+const numbers = [1, 2, 3, 4, 5];
+
+const double = (arr) => arr.map((num) => num * 2);
 
 
 /*
@@ -26,3 +29,9 @@ function squareAndFindEvens(numbers{
 */
 
 //ES2015 Arrow Function Shorthand
+
+const squareAndFindEvens = (numbers) => {
+  let squares = numbers.map((num) => num ** 2);
+  let evens = squares.filter((square) => square % 2 === 0);
+  return evens;
+};
