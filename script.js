d80622a27ad7ca7493c99600e51780063655ee31
Pike1868 <luis_14mendoza@protonmail.com>
2023-04-17 10:03:41 -0400

    Arrow functions exercise, refactoring old code, initial commit without answers

---
+++ script.js
@@ -0,0 +1,28 @@
+/*
+// ES5 Map callback
+function double(arr){
+    return arr.map(function(val){
+        return val * 2;
+    });
+}
+*/
+
+//ES2015 Arrow Function Shorthand
+
+
+/*
+//Refactor the following function to use arrow functions:
+
+function squareAndFindEvens(numbers{
+    var squares = numbers.map(function(num){
+        return num ** 2;
+    });
+    var evens = square.filter(function(square){
+        return square % 2 === 0;
+    });
+    return evens;
+})
+
+*/
+
+//ES2015 Arrow Function Shorthand
